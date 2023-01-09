6080f2af0713663cd56c2eb5e25557a0c1a9bf06
Dav Hunyah <xgarhontx@gmail.com>
2023-01-08 20:46:08 -0800

    assetmanager can load and play audio oneshots.

--- assetmanager.js
+++ assetmanager.js
@@ -18,30 +18,73 @@ class AssetManager {
     downloadAll(callback) {
         if (this.downloadQueue.length === 0) setTimeout(callback, 10);
         for (let i = 0; i < this.downloadQueue.length; i++) {
-            const img = new Image();
+            let path = this.downloadQueue[i];
+            console.log("Loading" + path);
+            switch (path.substring(path.length-3)) {
+                case "bmp":
+                case "gif":
+                case "jpg":
+                case "png":
+                    const img = new Image();
 
-            const path = this.downloadQueue[i];
-            console.log(path);
+                    img.addEventListener("load", () => {
+                        console.log("Loaded " + img.src);
+                        this.successCount++;
+                        if (this.isDone()) callback();
+                    });
 
-            img.addEventListener("load", () => {
-                console.log("Loaded " + img.src);
-                this.successCount++;
-                if (this.isDone()) callback();
-            });
+                    img.addEventListener("error", () => {
+                        console.log("Error loading " + img.src);
+                        this.errorCount++;
+                        if (this.isDone()) callback();
+                    });
 
-            img.addEventListener("error", () => {
-                console.log("Error loading " + img.src);
-                this.errorCount++;
-                if (this.isDone()) callback();
-            });
+                    img.src = path;
+                    this.cache[path] = img;
+                    break;
+                case "ogg":
+                case "wav":
+                case "mp3":
+                    const aud = new Audio();
 
-            img.src = path;
-            this.cache[path] = img;
+                    aud.addEventListener("loadeddata", () => {
+                        console.log("Loaded " + aud.src);
+                        this.successCount++;
+                        if (this.isDone()) callback();
+                    });
+
+                    aud.addEventListener(" ", () => {
+                        console.log("Error loading " + aud.src);
+                        this.errorCount++;
+                        if (this.isDone()) callback();
+                    });
+
+                    aud.addEventListener("ended", () => {
+                        aud.pause();
+                        aud.currentTime = 0;
+                    });
+
+                    aud.src = path;
+                    aud.load();
+                    this.cache[path] = aud;
+                    break;
+            }
         }
     };
 
     getAsset(path) {
         return this.cache[path];
     };
-};
 
+    playAsset(path) {
+        let audio = this.cache[path];
+        audio.currentTime = 0;
+        audio.play();
+    };
+
+    playAsset(path, startTime) {
+        let audio = this.cache[path];
+        audio.currentTime = startTime; //untested
+        audio.play();
+    };
+};
