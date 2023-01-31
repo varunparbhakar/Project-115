608e70c1e079a5feeddb1c211cdbd225f0a500cf
Dav Hunyah <xgarhontx@gmail.com>
2023-01-31 13:41:41 -0800

    update & render loop overhauled with layers. (fixed Player to Door collision)

--- Scripts/GameObject/Player.js
+++ Scripts/GameObject/Player.js
@@ -298,21 +298,21 @@ class Player extends GameObject {
                 //interact
                 if (GAME_ENGINE.key_use && this.player_Collision_World_BB.collide(entity.bb_interact)) {
                     entity.repair()
-                } else
-                if (entity instanceof Door) {
-                    //movement
-                    if (entity.isLocked == true) {
-                        this.checkBBandPushOut(this.player_Collision_World_BB, this.last_collision_World_R, entity.bb)
-                    }
-                    //interact
-                    if (GAME_ENGINE.key_use && this.player_Collision_World_BB.collide(entity.bb_interact)) {
-                        if (this.money >= entity.cost) { //check money and buy
-                            this.money -= entity.cost
-                            entity.buy()
-                        }
+                }
+            } else if (entity instanceof Door) {
+                //movement
+                if (entity.isLocked == true) {
+                    this.checkBBandPushOut(this.player_Collision_World_BB, this.last_collision_World_R, entity.bb)
+                }
+                //interact
+                if (GAME_ENGINE.key_use && this.player_Collision_World_BB.collide(entity.bb_interact)) {
+                    if (this.money >= entity.cost) { //check money and buy
+                        this.money -= entity.cost
+                        entity.buy()
                     }
                 }
             }
+
         })
         // //Zombies
         // GAME_ENGINE.ent_MapObjects.forEach(() => {
